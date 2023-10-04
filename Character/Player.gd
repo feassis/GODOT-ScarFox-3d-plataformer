e16f5ec4689b774dff9475140856cfc0a0fda58d
felipe assis <felipe.assis.2002@gmail.com>
2023-10-04 19:50:51 -0300

    initial commit

---
+++ Character/Player.gd
@@ -0,0 +1,50 @@
+extends CharacterBody3D
+
+@export_category("Setup")
+@export var normalSpeed: float = 5.0
+@export var sprintSpeed: float = 9.0
+@export var JUMP_VELOCITY: float = 4.5
+
+var _currentSpeed: float = normalSpeed
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+@export_category("Objects")
+@export var body: Node3D = null
+@export var springArmOffset: Node3D = null
+
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	# Handle Jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
+	direction = direction.rotated(Vector3.UP, springArmOffset.rotation.y)
+	
+	if direction:
+		velocity.x = direction.x * _currentSpeed
+		velocity.z = direction.z * _currentSpeed
+		body.apply_rotation(velocity.normalized())
+	else:
+		velocity.x = move_toward(velocity.x, 0, _currentSpeed)
+		velocity.z = move_toward(velocity.z, 0, _currentSpeed)
+
+	move_and_slide()
+	body.animate(velocity)
+	
+func is_running() -> bool:
+	if Input.is_action_pressed("run"):
+		_currentSpeed = sprintSpeed
+		return true
+	_currentSpeed = normalSpeed
+	return false
---
+++ Character/scripts/CharacterBody.gd
@@ -0,0 +1,25 @@
+extends Node3D
+class_name Body
+
+@export_category("Setup")
+@export var rotationVelocity: float = 0.15
+
+@export_category("Objects")
+@export var character: CharacterBody3D = null
+@export var animation: AnimationPlayer = null
+
+func apply_rotation(velocity: Vector3) -> void:
+	var angularVelocity:float = atan2( velocity.x, velocity.z)
+	rotation.y = lerp_angle(
+		rotation.y,  angularVelocity, rotationVelocity
+	)
+
+
+func animate(velocity: Vector3) -> void:
+	if velocity:
+		if character.is_running():
+			animation.play("Running_A")
+			return
+		animation.play("Walking_A")
+		return
+	animation.play("Idle")
