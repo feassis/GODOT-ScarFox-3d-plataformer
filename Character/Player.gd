fb8da3d3a664a174fa9ab3d7481873544f630ffe
felipe assis <felipe.assis.2002@gmail.com>
2023-10-10 16:38:50 -0300

    adding variable jump velocity

--- Character/Player.gd
+++ Character/Player.gd
@@ -1,20 +1,26 @@
 extends CharacterBody3D
 class_name Player
 
-@export_category("Setup")
+@export_category("Setup -> Movement")
 @export var normalSpeed: float = 5.0
 @export var sprintSpeed: float = 9.0
-@export var JUMP_VELOCITY: float = 4.5
 @export var deacelerationOnAir: float = 1.0
 @export var deacelerationOnFloor: float = 15.0
 @export var onAirDamping: float = 0.3
+
+@export_category("Setup -> Jump")
+@export var JUMP_VELOCITY: float = 4.5
 @export var cayoteTimeDuration: float = 0.5
+@export var gravityIncrease: float = 9.8
+@export var jumpButtonGracePeriod: float = 0.5
 
 var _currentSpeed: float = normalSpeed
 var onJumpStartSpeed: float = 0
 var isOnCayoteTime: bool = false
 var cayoteTimeExpired: bool = false
 var cayoteTimer: float = 0
+var jumpButtonIsPressed: bool = false
+var jumpButtonGraceTimer = 0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -37,7 +43,17 @@ func _physics_process(delta):
 
 	# Handle Jump.
 	if CanJump() and Input.is_action_just_pressed("jump"):
+		jumpButtonIsPressed = true
 		Jump()
+		
+	if jumpButtonIsPressed:
+		jumpButtonGraceTimer += delta
+		if jumpButtonGraceTimer > jumpButtonGracePeriod:
+			jumpButtonIsPressed = false
+			jumpButtonGraceTimer = 0
+	
+	if Input.is_action_just_released("jump"):
+		jumpButtonIsPressed = false
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -45,8 +61,6 @@ func _physics_process(delta):
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	
 	direction = direction.rotated(Vector3.UP, springArmOffset.rotation.y)
-	
-	
 	
 	if not is_on_floor():
 		_currentSpeed *= onAirDamping
@@ -83,9 +97,16 @@ func Jump() -> void:
 	onJumpStartSpeed = _currentSpeed
 	
 func HandleFallingLogic(delta) -> void:
+	var gravityOnBody: float
+	
+	if jumpButtonIsPressed:
+		gravityOnBody = gravity
+	else: 
+		gravityOnBody = gravity + gravityIncrease
+	
 	# Add the gravity.
 	if not is_on_floor():
-		velocity.y -= gravity * delta
+		velocity.y -= gravityOnBody * delta
 		if not isOnCayoteTime and not cayoteTimeExpired:
 			isOnCayoteTime = true
 		if isOnCayoteTime:
